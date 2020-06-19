6127ddf91d869bebea4e97cd10de1eb020f64f74
alok-shete <shetealok@gmail.com>
2020-06-19 08:18:14 +0530

    Updates

---
+++ precache-manifest.7cb005760acf71f7000a62525c448c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b950a692309df95343161e760eb0e9a3",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "de7c44468a211f9f5efc",
+    "url": "/tic-tac-toe/static/css/main.31c21777.chunk.css"
+  },
+  {
+    "revision": "46b533efbe7a51b6ffc6",
+    "url": "/tic-tac-toe/static/js/2.38ed0603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.38ed0603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7c44468a211f9f5efc",
+    "url": "/tic-tac-toe/static/js/main.9bb331e6.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
